9619066952f58bb31bc911b43bfa5bcda74e92ad
hnsfq <93572667+hnsfq@users.noreply.github.com>
2024-09-07 13:50:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -12,6 +12,7 @@ let addresses = [
 // 设置优选地址api接口
 let addressesapi = [
 	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
+	'https://cf-workers-text2kv-7zp.pages.dev/ip.txt?token=mxl', 
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
 ];
 
